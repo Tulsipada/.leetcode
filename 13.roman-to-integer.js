bf8463e9e4798b8f23e7cb7b64b2ea518f876476
Tulsipada Das <tulsipada55@gmail.com>
2023-04-30 20:46:50 +0530

    solve

--- 13.roman-to-integer.js
+++ 13.roman-to-integer.js
@@ -10,6 +10,21 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    symbols = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    };
+    
+    value = 0;
+    for(let i = 0; i < s.length; i+=1){
+        symbols[s[i]] < symbols[s[i+1]] ? value -= symbols[s[i]]: value += symbols[s[i]]
+    }
+    return value
     
 };
 // @lc code=end
